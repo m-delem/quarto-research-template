f9431b0c341e4062d6eb090d8336dd1daab1a6b5
Maël Delem <mael.a.delem@gmail.com>
2024-07-07 18:41:15 +0200

    pacman for the win?

--- scripts/_setup.R
+++ scripts/_setup.R
@@ -1,24 +1,36 @@
-# don't forget to use `renv` for a reproducible environment!
+
+# renv --------------------------------------------------------------------
+
+# Don't forget to use `renv` for a reproducible environment!
 # install.packages("renv")
 # library("renv")
 # renv::init() was used to create the renv.lock file in this template
 # Now each time you open the project, you can restore the environment with:
 renv::restore()
 
-# librarian allows to check/install/load packages with a single call
-if (!require("librarian")) install.packages("librarian")
-library(librarian)
-shelf(
-  here,      # setting the root directory
-  see,       # theme_modern and okabeito
-  report,    # reporting various info
+
+# Packages ----------------------------------------------------------------
+
+# pacman allows to check/install/load packages with a single call
+# if (!require("pacman")) install.packages("pacman") # already in renv.lock
+library("pacman")
+
+# package names
+pacman::p_load(
+  here,      # easy file paths
+  see,       # theme_modern and okabeito palette
+  report,    # reporting various info 
   labelled,  # labelled data
   tidyverse  # modern R ecosystem
 )
 
-source(here("scripts/_functions.R")) # custom functions shared across scripts
 
-# ─── Global cosmetic theme ───────────
+# Custom functions shared across scripts ----------------------------------
+source(here("scripts/_functions.R"))
+
+
+# Global cosmetic theme ---------------------------------------------------
+
 theme_set(theme_modern(base_size = 14)) # from see in easystats
 
 # setting my favourite palettes as ggplot2 defaults
@@ -29,8 +41,9 @@ options(
   ggplot2.continuous.fill   = scale_fill_viridis_c
 )
 
-# ─── Fixing a seed for reproducibility ───
+
+# Fixing a seed for reproducibility ────────────────────────────────────────
 set.seed(14051998)
 
-# ─── Adding all packages' citations to a .bib ───
+# Adding all packages' citations to a .bib --------------------------------
 knitr::write_bib(c(.packages()), file = here("bibliography/packages.bib"))
