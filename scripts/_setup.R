d1063f94cc47fb1a4f892d554ef0af8d3495dffc
Maël Delem <mael.a.delem@gmail.com>
2024-10-30 15:25:32 +0100

    reducing the scope since the template might change drastically

--- scripts/_setup.R
+++ scripts/_setup.R
@@ -1,22 +1,3 @@
-
-# Renv --------------------------------------------------------------------
-
-# Don't forget to use `renv` for a reproducible environment!
-# see https://rstudio.github.io/renv/articles/renv.html for more details
-
-# install.packages("renv")  # if you don't have it yet
-# library("renv")           # same as above
-
-# renv::init() has already been used to create the renv.lock file (the file that
-# contains the exact details of the packages you used) in this template, so now 
-# the project only needs to be restored each time you start working. You will
-# be asked to install the packages that I added down below upon running this 
-# script, but you can change this to suit your needs.
-renv::restore()
-
-
-# Packages ----------------------------------------------------------------
-
 # pacman allows to check/install/load packages with a single call
 if (!requireNamespace("pacman")) install.packages("pacman")
 
@@ -25,7 +6,6 @@ pacman::p_load(
   here,        # easy file paths
   sessioninfo # reporting various info 
 )
-
 
 # Fixing a seed for reproducibility
 set.seed(123)
