8a6887b7e6e8aa210d16fe9fc73e2132efd0b216
Maël Delem <mael.a.delem@gmail.com>
2024-11-15 03:25:19 +0100

    source-all out, fs and purrr in

--- R/source-all.R
+++
@@ -1,7 +0,0 @@
-# function to source all files in a directory
-# 
-source_all <- function(path = "/", pattern = NULL) {
-  files <- list.files(path, pattern, full.names = TRUE, recursive = TRUE)
-  # the assignment to "silence" prevents the output from being printed
-  silence <- lapply(files, function(x) source(file = x, verbose = FALSE))
-}
